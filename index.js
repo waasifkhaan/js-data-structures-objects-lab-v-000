ac464bcdd838e038dd74efefdd34a613b220f9a0
Wasif Khan <waasif.khaan@gmail.com>
2018-11-02 04:48:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  
-  
+  driver[key] = value;
+  return driver;
+
 }
